3ad73d10f7a5e4f9b875e60c0fcf15a0566d7156
Augustin Bouverot <augustin.bouverot@gmail.com>
2025-03-14 20:57:09 +0100

    tests: add automated blarggs cpu_instrs tests

---
+++ tests/blarggs/run.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+PROJECT=xenogb
+
+TEST_DIR=$(dirname $(realpath -s $0))
+ROOT=$TEST_DIR/../..
+EXEC=$ROOT/target/debug/$PROJECT
+
+echo "# Compiling $PROJECT"
+cargo build
+
+failed=0
+
+echo "# Run blargg's tests"
+for file in $TEST_DIR/roms/*; do
+    cmd="$EXEC -c $file";
+    if timeout --foreground 2 $EXEC -c "$file" 2>&1 | grep -q "Passed"; then
+        echo "$cmd --- OK";
+    else
+        echo "$cmd --- FAILED";
+        failed=1;
+    fi
+done;
+
+exit $failed
