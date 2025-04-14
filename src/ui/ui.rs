ae0146bf646f47c2c780dab0ce02ffb7bf1f37a7
Augustin Bouverot <augustin.bouverot@gmail.com>
2025-04-14 22:26:49 +0200

    add bsod

--- src/ui/ui.rs
+++ src/ui/ui.rs
@@ -8,7 +8,11 @@ use crate::run_emu::{run_emu_thread, EmuState};
 use cphf::{phf_ordered_map, OrderedMap};
 use crossbeam_channel::{Receiver, Sender};
 use eframe::egui;
-use egui::{Context, Id, Key, Modal, RichText, ScrollArea, ViewportBuilder, ViewportCommand};
+use egui::{
+    include_image, CentralPanel, Color32, Context, CornerRadius, FontId, Frame, Hyperlink, Image,
+    Key, Margin, RichText, ScrollArea, ViewportBuilder,
+};
+use egui_extras::install_image_loaders;
 
 use std::path::PathBuf;
 
@@ -91,6 +95,8 @@ impl XenoGBUI {
 
         let debugger = DebuggerUi::new(ctx, debug, dbg_commands_sd.clone(), dbg_data_rc);
 
+        install_image_loaders(&ctx.egui_ctx);
+
         Self {
             screen_buffer,
             screen_texture,
@@ -120,29 +126,91 @@ impl XenoGBUI {
         }
     }
 
-    fn emulation_crash(&self, ctx: &Context) {
+    fn bsod(&self, ctx: &Context) {
         let crash = self.emu_state.crash_info();
 
-        Modal::new(Id::new("crash-popup")).show(ctx, |ui| {
-            ui.label(format!("Emulation crashed: {}", crash.reason));
-            ui.collapsing("Backtrace", |ui| {
-                ScrollArea::vertical().max_height(200.0).show(ui, |ui| {
-                    ui.set_min_width(500.0);
-                    ui.code(RichText::from(crash.backtrace.clone()).monospace())
+        CentralPanel::default()
+            .frame(Frame::new().fill(Color32::from_rgb(0, 0x78, 0xd7)).inner_margin(Margin::from(20)))
+            .show(ctx, |ui| {
+                ui.style_mut().visuals.override_text_color = Some(Color32::WHITE);
+                let scale = ui.min_rect().width() / WINDOW_SIZE[0];
+
+                ui.vertical(|ui| {
+                    ui.label(
+                        RichText::new(":(")
+                            .size(72.0 * scale)
+                            .font(FontId::proportional(72.0 * scale)),
+                    );
+
+                    ui.add_space(30.0 * scale);
+
+                    ui.label(
+                        RichText::new("Your emulator ran into a problem and needs to restart.")
+                            .size(22.0 * scale)
+                    );
+
+                    ui.add_space(40.0 * scale);
+
+                    ui.horizontal(|ui| {
+                        ui.vertical(|ui| {
+                            ui.add(Image::new(include_image!("assets/bsod.png")).fit_to_original_size(scale * 0.35));
+                            ui.add_space(200.0);
+                        });
+
+                        ui.vertical(|ui| {
+                            ui.vertical(|ui| {
+                                ui.spacing_mut().item_spacing.y = 0.0;
+                                ui.label(
+                                    RichText::new("For more information about this issue and possible fixes, visit ")
+                                        .size(14.0 * scale)
+                                );
+                                ui.add(
+                                    Hyperlink::from_label_and_url(
+                                        RichText::new("https://github.com/Sacrimento/xenogb")
+                                            .size(14.0 * scale),
+                                        "https://github.com/Sacrimento/xenogb"
+                                    )
+                                );
+                            });
+
+                            ui.add_space(10.0 * scale);
+
+                            ui.label(
+                                RichText::new(format!("If you call a support person (please don't), give them this info:\n"))
+                                    .size(14.0 * scale)
+                            );
+
+                            ui.collapsing(
+                                RichText::new(format!("Panicked: {}", crash.reason))
+                                    .size(14.0 * scale),
+                                |ui| {
+                                    ScrollArea::vertical()
+                                        .show(ui, |ui| {
+                                            Frame::new()
+                                                .fill(Color32::from_rgb(0, 0x5b, 0xa5))
+                                                .corner_radius(CornerRadius::same(6))
+                                                .show(ui, |ui| {
+                                                    let bt = RichText::new(crash.backtrace.clone())
+                                                        .monospace()
+                                                        .size(14.0 * scale);
+
+                                                    ui.label(bt);
+                                                });
+                                        });
+                                },
+                            );
+                        });
+                     });
                 });
+
             });
-            if ui.button("Ok").clicked() {
-                ctx.send_viewport_cmd(ViewportCommand::Close);
-            }
-        });
     }
 }
 
 impl eframe::App for XenoGBUI {
     fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
         if self.emu_state.is_dead() {
-            self.emulation_crash(ctx);
-            return;
+            return self.bsod(ctx);
         }
 
         ctx.input(|inp| {
