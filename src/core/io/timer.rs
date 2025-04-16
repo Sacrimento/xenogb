bc64170dd781712bbf57f2d091c7113e175492c5
Augustin Bouverot <augustin.bouverot@gmail.com>
2025-04-16 18:57:22 +0200

    rework timer

--- src/core/io/timer.rs
+++ src/core/io/timer.rs
@@ -1,7 +1,4 @@
-use crate::core::cpu::{
-    interrupts::{request_interrupt, InterruptFlags},
-    CLOCK_SPEED,
-};
+use crate::core::cpu::interrupts::{request_interrupt, InterruptFlags};
 
 pub struct Timer {
     div: u16,
@@ -9,47 +6,54 @@ pub struct Timer {
     tma: u8,
     tac: u8,
 
-    ticks_since_inc: u32,
+    prev_div_bit: bool,
 }
 
 impl Timer {
     pub fn new() -> Self {
         Self {
-            div: 0xac00,
+            div: 0,
             tima: 0,
             tma: 0,
             tac: 0,
-            ticks_since_inc: 0,
+            prev_div_bit: false,
         }
     }
 
     pub fn tick(&mut self, cycles: u8) {
-        self.div = self.div.wrapping_add(cycles as u16);
+        self.div = self.div.wrapping_add((cycles as u16) * 4);
 
-        if (self.tac & (1 << 2)) == 0 {
-            return;
+        let current_bit = self.div_bit();
+
+        if self.timer_enabled() && self.prev_div_bit && !current_bit {
+            self.inc_tima();
         }
 
-        self.ticks_since_inc += cycles as u32 * 4;
+        self.prev_div_bit = current_bit;
+    }
 
-        let ticks_per_inc: u32 = match self.tac & 0b11 {
-            0b00 => CLOCK_SPEED / 4096,
-            0b01 => CLOCK_SPEED / 262144,
-            0b10 => CLOCK_SPEED / 65536,
-            0b11 => CLOCK_SPEED / 16384,
+    fn div_bit(&self) -> bool {
+        let bit_idx = match self.tac & 0b11 {
+            0b00 => 9,
+            0b01 => 3,
+            0b10 => 5,
+            0b11 => 7,
             _ => unreachable!(),
         };
+        (self.div >> bit_idx) & 1 == 1
+    }
 
-        if self.ticks_since_inc >= ticks_per_inc {
-            self.ticks_since_inc %= ticks_per_inc;
-            self.tima += 1;
+    fn inc_tima(&mut self) {
+        self.tima = self.tima.wrapping_add(1);
 
-            if self.tima == 0xff {
-                self.tima = self.tma;
-
-                request_interrupt(InterruptFlags::TIMER);
-            }
+        if self.tima == 0 {
+            self.tima = self.tma;
+            request_interrupt(InterruptFlags::TIMER);
         }
+    }
+
+    fn timer_enabled(&self) -> bool {
+        self.tac & 0b100 != 0
     }
 
     pub fn read(&self, addr: u16) -> u8 {
@@ -57,17 +61,34 @@ impl Timer {
             0xff04 => (self.div >> 8) as u8,
             0xff05 => self.tima,
             0xff06 => self.tma,
-            0xff07 => self.tac,
+            0xff07 => self.tac | 0xf8,
             _ => unreachable!(),
         }
     }
 
     pub fn write(&mut self, addr: u16, value: u8) {
         match addr {
-            0xff04 => self.div = 0,
+            0xff04 => {
+                if self.timer_enabled() && self.div_bit() {
+                    self.inc_tima();
+                }
+
+                self.div = 0;
+                self.prev_div_bit = false;
+            }
             0xff05 => self.tima = value,
             0xff06 => self.tma = value,
-            0xff07 => self.tac = value,
+            0xff07 => {
+                let old_bit = self.timer_enabled() && self.div_bit();
+                self.tac = value & 0b111;
+                let new_bit = self.timer_enabled() && self.div_bit();
+
+                if old_bit && !new_bit {
+                    self.inc_tima();
+                }
+
+                self.prev_div_bit = new_bit;
+            }
             _ => unreachable!(),
         };
     }
