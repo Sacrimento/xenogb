6c1af417747cca985386576cb6aca9ca131e1787
Sacrimento <augustin.bouverot@gmail.com>
2026-06-16 22:53:47 +0200

    fix ci

--- tests/blarggs/run.sh
+++ tests/blarggs/run.sh
@@ -4,7 +4,9 @@ PROJECT=xenogb
 
 TEST_DIR=$(dirname $(realpath -s $0))
 ROOT=$TEST_DIR/../..
-EXEC=$ROOT/target/debug/$PROJECT
+EXEC=$ROOT/target/release/$PROJECT
+# Last blarggs test takes ~20s (https://github.com/retrio/gb-test-roms/blob/c240dd7d700e5c0b00a7bbba52b53e4ee67b5f15/cpu_instrs/source/11-op%20a%2C(hl).s#L2)
+TIMEOUT=25
 
 failed=0
 tests=""
@@ -12,7 +14,7 @@ tests=""
 echo "# Run blargg's tests"
 for file in $TEST_DIR/roms/*; do
     test_case=$(basename $file)
-    (timeout --foreground 15 $EXEC --headless -s -c "$file" 2>&1 | grep -q "Passed") &
+    (timeout --foreground $TIMEOUT $EXEC --headless -s -c "$file" 2>&1 | grep -q "Passed") &
     tests+=" $test_case:$!"
 done;
 
